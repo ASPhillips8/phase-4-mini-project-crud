0501ffac23bed986141a935859dcd4a35e1ee3ea
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-07 09:51:24 -0400

    Define destory and updated route

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -11,16 +11,21 @@ class SpicesController < ApplicationController
   end
 
   def update
-    spice = Spice.find(params[:id])
+    spice = find_spice
     spice.update(spice_params)
     render json: spice
   end
 
   def destroy
-
+    spice = find_spice
+    spice.destroy
   end
 
   private
+
+  def find_spice
+    Spice.find(params[:id])
+  end
 
   def spice_params
     params.permit(:title, :image, :description, :notes, :rating)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :spices, only: [:index, :create, :update, :destroy]
+  resources :spices, except: [:show]
 end
